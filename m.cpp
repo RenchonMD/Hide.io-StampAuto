010c2c4a4114e851b0e162179feabbb2d00e2c37
Renchon <92147511+RenchonMD@users.noreply.github.com>
2023-02-25 11:09:59 +0900

    Update m.cpp

--- m.cpp
+++ m.cpp
@@ -1,5 +1,4 @@
 bool StampAuto = false;
-
 void (*Emotion)(void *instance, int i);
 void (*old_Stamp)(void *instance);
 void Stamp(void *instance) {
@@ -10,6 +9,5 @@ void Stamp(void *instance) {
     }
     old_Stamp(instance);
 }
-
 HOOK_LIB("libil2cpp.so", "Offset", Stamp, old_Stamp);   
 Emotion = (void(*)(void *,int))getAbsoluteAddress("libil2cpp.so", Offset);
