e7b483236f0aada5d1fdb7a53ec02c78746110a8
れんちょん <92147511+RenchonMD@users.noreply.github.com>
2023-03-19 11:12:01 +0900

    Update and rename m.cpp to Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,5 @@
 bool StampAuto = false;
+
 void (*Emotion)(void *instance, int i);
 void (*old_Stamp)(void *instance);
 void Stamp(void *instance) {
@@ -9,5 +10,6 @@ void Stamp(void *instance) {
     }
     old_Stamp(instance);
 }
+
 HOOK_LIB("libil2cpp.so", "Offset", Stamp, old_Stamp);   
 Emotion = (void(*)(void *,int))getAbsoluteAddress("libil2cpp.so", Offset);
